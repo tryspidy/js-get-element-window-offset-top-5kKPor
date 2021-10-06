6e38dcb68d949916ee8658eb06c1563813defc91
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 01:39:20 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var element = document.getElementById('myElement');
+var topPos = element.getBoundingClientRect().top + window.scrollY;
+var leftPos = element.getBoundingClientRect().left + window.scrollX;
